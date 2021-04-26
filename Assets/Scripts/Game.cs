ad3f4d93594844d4aaca982aab3a300366a9049e
Greer Hoffmann <ghoffmann@Amelias-MacBook-Air.local>
2021-04-26 00:03:36 -0400

    two pieces show on the board

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    //Reference from Unity IDE
+    public GameObject piece;
+
+    // Matrices needed, positions of each of the GameObjects
+    // Also separate arrays for the players in order to easily keep track of them all
+    private GameObject[,] positions = new GameObject[8, 8]; // internal game positions (not unity coordinates)
+    private GameObject[] playerA = new GameObject[1]; // dummy for now
+    private GameObject[] playerB = new GameObject[1];
+
+    private string currentPlayer = "A"; // current turn
+
+    private bool gameOver = false; // Game Ending
+
+    // create pieces and set their positions
+    void Start()
+    {
+        playerA = new GameObject[]{Create("A_normal", 0,0)};
+        playerB = new GameObject[]{Create("B_normal", 0,7)};
+
+        for (int i = 0; i<playerA.Length; i++){
+            setPosition(playerA[i]);
+            setPosition(playerB[i]);
+        }
+    }
+
+    public GameObject Create(string name, int x, int y)
+    {
+        GameObject obj = Instantiate(piece, new Vector3(0, 0, 1), Quaternion.identity);
+        Piece p = obj.GetComponent<Piece>();
+        p.name = name; // name is a built in variable in unity
+        p.SetXBoard(x);
+        p.SetYBoard(y);
+        p.Activate();
+        return obj;
+    }
+
+    public void setPosition(GameObject obj)
+    {
+        Piece p = obj.GetComponent<Piece>();
+
+        //Overwrites either empty space or whatever was there
+        positions[p.GetXBoard(), p.GetYBoard()] = obj;
+    }
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    //References to objects in our Unity Scene
+    public GameObject controller;
+    public GameObject moveDot;
+
+    //Position for this piece on the Board
+    //The correct position will be set later
+    private int xBoard = -1;
+    private int yBoard = -1;
+
+    //Variable for keeping track of the player it belongs to player A or player B
+    private string player;
+
+    //References to all the possible Sprites that this Chesspiece could be
+    public Sprite A_normal, A_plus;
+    public Sprite B_normal, B_plus;
+
+    public void Activate(){
+
+        controller = GameObject.FindGameObjectWithTag("GameController"); // Get the game controller
+
+        SetCoords(); // Take the instantiated location and adjust transform
+
+        // switch on sprite name
+        switch(this.name){
+            case "A_normal": this.GetComponent<SpriteRenderer>().sprite = A_normal; player = "A"; break;
+            case "B_normal": this.GetComponent<SpriteRenderer>().sprite = B_normal; player = "B"; break;
+            case "A_plus": this.GetComponent<SpriteRenderer>().sprite = A_plus; player = "A"; break;
+            case "B_plus": this.GetComponent<SpriteRenderer>().sprite = B_plus; player = "B"; break;
+        }
+    }
+
+    public void SetCoords() // correspond unity coordinates with in-game coordinates
+    {
+        //Get the board value in order to convert to xy coords
+        float x = xBoard;
+        float y = yBoard;
+
+        //Adjust by variable offset
+        x *= 0.66f;
+        y *= 0.66f;
+
+        //Add constants (pos 0,0)
+        x += -2.3f;
+        y += -2.3f;
+
+        //Set actual unity values
+        this.transform.position = new Vector3(x, y, 1.0f);
+    }
+
+    // Getter and Setter methods from Unity Chess Tutorial
+    public int GetXBoard()
+    {
+        return xBoard;
+    }
+
+    public int GetYBoard()
+    {
+        return yBoard;
+    }
+
+    public void SetXBoard(int x)
+    {
+        xBoard = x;
+    }
+
+    public void SetYBoard(int y)
+    {
+        yBoard = y;
+    }
+}
